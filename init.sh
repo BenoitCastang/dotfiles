7221c975249fa2b2ccee6982a9820a25d0726082
Benoit Castang <castang.benoit@gmail.com>
2024-11-08 11:53:35 +0100

    remove previous bashrc

--- init.sh
+++ init.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 if [[ "$(whoami)" == "root" ]]; then
+	rm /root/.bashrc
 	ln -s /root/dotfiles/.bashrc /root/.bashrc
 	. /root/.bashrc
 else
+	rm /home/"$USER"/.bashrc
 	ln -s /home/"$USER"/dotfiles/.bashrc /home/"USER"/.bashrc
 	. /home/"$USER"/.bashrc 
 fi
