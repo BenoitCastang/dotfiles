2470e27778e16e3557220df9c5b6afc9d4ed3e3f
BenoitCastang <castang.benoit@gmail.com>
2026-03-27 03:43:34 +0100

    auto lsp installation lazy vim

---
+++ nvim/lua/plugins/lsp.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    servers = {
+      ts_ls = {},
+      bashls = {},
+      html = {},
+      cssls = {},
+      emmet_language_server = {},
+      phpactor = {},
+    },
+  },
+}
