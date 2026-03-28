d1965b404a409d7d162585d403e6ae86dab6782d
BenoitCastang <castang.benoit@gmail.com>
2026-03-28 04:33:31 +0100

    sync folds

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,13 +1,13 @@
-vim.api.nvim_create_autocmd("FileType", { -- disable spell checking
+vim.api.nvim_create_autocmd("FileType", { -- markdowns: disable spell checking, set manuals folds
   pattern = { "markdown", "text" },
   callback = function()
     vim.opt_local.spell = false
-    vim.cmd('set foldmethod=manual')
+    vim.opt_local.foldmethod = "manual"
     vim.cmd('loadview')
   end,
 })
 
-vim.api.nvim_create_autocmd({ "BufDelete", "BufUnload" }, { --
+vim.api.nvim_create_autocmd({ "BufDelete", "BufUnload" }, { -- markdowns: save folds
   pattern = "*.md",
   callback = function()
     vim.cmd("normal! zM")
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,1 +1,2 @@
 vim.opt.foldlevelstart = 0
+vim.opt.viewdir = vim.fn.expand("~/dotfiles/nvim/view") -- save folds
